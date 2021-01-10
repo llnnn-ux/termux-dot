a3c44405a62976099176cade36e2104b8235e543
llnnn-ux <72482919+llnnn-ux@users.noreply.github.com>
2021-01-10 17:25:56 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
+echo "Installing packages..."
 pkg i -y openssh nano python nyancat git wget zsh neofetch fish curl proot file aria2 p7zip neovim bat exa dnsutils
-pip install -U youtube-dl setuptools pip 
+echo "Installing pip packages..."
+pip install -U youtube-dl setuptools pip
+echo "Cloning zsh plugins..." 
 cd ~; mkdir .zsh; cd .zsh
 git clone https://github.com/zsh-users/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions
@@ -9,6 +12,7 @@ git clone --depth=1 https://github.com/denysdovhan/spaceship-prompt
 cd ~
 git clone https://github.com/llnnn-ux/termux-dot
 mkdir ~/.termux
+echo "Linking dotfiles..."
 ln -sf ~/termux-dot/.termux/termux.properties ~/.termux/termux.properties
 ln -sf ~/termux-dot/.aliasrc ~/.aliasrc
 ln -sf ~/termux-dot/.zprofile ~/.zprofile
@@ -17,3 +21,4 @@ ln -sf ~/termux-dot/.gitconfig ~/.gitconfig
 ln -sf ~/termux-dot/.vimrc ~/.vimrc
 mkdir $PREFIX/share/zsh/site-functions; ln -sf ~/.zsh/spaceship-prompt/spaceship.zsh $PREFIX/share/zsh/site-functions/prompt_spaceship_setup
 chsh -s zsh
+echo "Done!"
